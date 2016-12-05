dd2892a551348620abd26ac4c960643974e6ab70
James Coll <jcoll@scss.tcd.ie>
2016-12-05 17:35:15 +0000

    up to 114 but properties messages are not being read

--- src/main/java/masterSpringMvc/profile/ProfileController.java
+++ src/main/java/masterSpringMvc/profile/ProfileController.java
@@ -7,24 +7,45 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import date.USLocalDateFormatter;
+import masterSpringMvc.date.USLocalDateFormatter;
+
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class ProfileController {
-	
+
 	@RequestMapping("/profile")
 	public String displayProfile(ProfileForm profileForm){
 		return "profile/profilePage";
 	}
-	
-	@RequestMapping(value="/profile", method=RequestMethod.POST)
-	public String saveProfile(ProfileForm profileForm){
+
+	@RequestMapping(value="/profile", params={"save"}, method=RequestMethod.POST)
+	public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult){
+		if (bindingResult.hasErrors()) {
+			return "profile/profilePage";
+		}
 		System.out.println("save ok " + profileForm);
 		return "redirect:/profile";
 	}
-	
+
 	@ModelAttribute("dateFormat")
-	public String localeFormat(Locale locale){
-		return USLocalDateFormatter.getPattern(locale);
+    public String localeFormat(Locale locale) {
+        return USLocalDateFormatter.getPattern(locale);
+    }
+
+	@RequestMapping(value = "/profile", params = {"addTaste"})
+	public String addRow(ProfileForm profileForm) {
+			profileForm.getTastes().add(null);
+			return "profile/profilePage";
+	}
+
+	@RequestMapping(value = "/profile", params = {"removeTaste"})
+	public String removeRow(ProfileForm profileForm, HttpServletRequest req) {
+			Integer rowId = Integer.valueOf(req.getParameter("removeTaste"));
+			profileForm.getTastes().remove(rowId.intValue());
+			return "profile/profilePage";
 	}
 }
--- src/main/java/masterSpringMvc/profile/ProfileForm.java
+++ src/main/java/masterSpringMvc/profile/ProfileForm.java
@@ -1,19 +1,34 @@
 package masterSpringMvc.profile;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import masterSpringMvc.date.PastLocalDate;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileForm {
-	
+
+	@Size(min = 2)
 	private String twitterHandle;
+	@Email
+	@NotEmpty
 	private String email;
+	@NotNull
+	@PastLocalDate
 	private LocalDate birthDate;
+	@NotEmpty
 	private List<String> tastes = new ArrayList<>();
-	
+
 	public String getTwitterHandle() {
 		return twitterHandle;
 	}
+
 	public void setTwitterHandle(String twitterHandle) {
 		this.twitterHandle = twitterHandle;
 	}
